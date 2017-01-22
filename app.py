a2824d20b0a27bc5e652dfff0143d6b721efd69a
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-01-21 19:43:39 -0500

    Added files

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from JSONEncoder import JSONEncoder
+from server import *
+
 app = Flask(__name__)
+
+
+db = server.get_db()
+
+@app.route("/logData",methods=['POST'])
+def logout():
+	db.userdata.insert(request.get_json())
+
+
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	app.run(host='0.0.0.0', port=port)
---
+++ server.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+
+def get_db():
+    client = MongoClient('mongodb://heroku_hgj2dkpq:192ledtpq5qhbs9ghpao0h2v2p@ds141078.mlab.com:41078/heroku_hgj2dkpq')
+    db = client.heroku_hgj2dkpq
+    return db
